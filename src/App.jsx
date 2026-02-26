92e40c12e73ec7a573baba9644694031fb6b89a7
Anshika Srivastava <srivastavaanshika510@gmail.com>
2026-02-26 13:09:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Services from "./Pages/Services";
@@ -27,36 +25,39 @@ import Page6 from "./Pages/TeamPage6";
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-w-[1280px] overflow-x-auto">
-        <Navbar />
-        <ScrollToTop />
+      <Navbar />
+       <ScrollToTop />
 
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/services-details" element={<ServicesDetails />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsCondition />} />
-            <Route path="/faq" element={<FAQs />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/security-approach" element={<SecurityApproach />} />
-            <Route path="/getstarted" element={<GetStarted />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/tp1" element={<Page1 />} />
-            <Route path="/tp2" element={<Page2 />} />
-            <Route path="/tp3" element={<Page3 />} />
-            <Route path="/tp4" element={<Page4 />} />
-            <Route path="/tp5" element={<Page5 />} />
-            <Route path="/tp6" element={<Page6 />} />
-          </Routes>
-        </main>
+    
+      <main className="pt-24">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services-details" element={<ServicesDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsCondition/>} />
+          <Route path="/faq" element={<FAQs/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/security-approach" element={<SecurityApproach />} />
+          <Route path="/getstarted" element={<GetStarted />} />
+          <Route path="/project" element={<Project/>} />
+          <Route path="/tp1" element={<Page1/>} />
+          <Route path="/tp2" element={<Page2/>} />
+          <Route path="/tp3" element={<Page3/>} />
+          <Route path="/tp4" element={<Page4/>} />
+          <Route path="/tp5" element={<Page5/>} />
+          <Route path="/tp6" element={<Page6/>} />
 
-        <Footer />
-      </div>
+            
+
+        </Routes>
+       
+      </main>
+       {/* Footer */}
+      <Footer/>
     </BrowserRouter>
   );
 }
